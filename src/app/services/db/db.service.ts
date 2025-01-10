db4845d5a0022975266011c15ec4840f686b57da
devMartinsDavid <david_wallison@hotmail.com>
2025-01-10 11:15:21 -0300

    create interface, dbservice

---
+++ src/app/services/db/db.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Expense } from '../../interfaces/expense.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbService {
+
+  constructor() { }
+  getNextId(): number {
+    const nextId = localStorage.getItem('id');
+    return nextId ? parseInt(nextId, 10) + 1 : 1;
+  }
+
+  // Grava uma nova despesa no localStorage
+  grave(expense: Expense): void {
+    const id = this.getNextId();
+    localStorage.setItem(id.toString(), JSON.stringify(expense));
+    localStorage.setItem('id', id.toString());
+  }
+
+  // Recupera todas as despesas armazenadas no localStorage
+  retrieveAllRecords(): Expense[] {
+    const expenses: Expense[] = [];
+    const id = localStorage.getItem('id');
+    const maxId = id ? parseInt(id, 10) : 0;
+
+    for (let i = 1; i <= maxId; i++) {
+      const expense = JSON.parse(localStorage.getItem(i.toString()) || 'null') as Expense;
+      if (expense === null) {
+        continue;
+      }
+      expense.id = i; // Adiciona o ID ao objeto de despesa
+      expenses.push(expense);
+    }
+
+    console.log('Despesas recuperadas:', expenses);
+    return expenses;
+  }
+
+  // Pesquisa despesas com base nos filtros fornecidos
+  search(filters: Partial<Expense>): Expense[] {
+    const allRecords = this.retrieveAllRecords();
+    return allRecords.filter(expense => {
+      return Object.keys(filters).every(
+        key => expense[key as keyof Expense] === filters[key as keyof Expense] || filters[key as keyof Expense] === ''
+      );
+    });
+  }
+
+  // Remove uma despesa pelo ID
+  remove(id: number): void {
+    localStorage.removeItem(id.toString());
+  }
+}
